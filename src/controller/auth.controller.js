89b57c6fbee2e76d1efac538c30367b04fcfd07c
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-03 09:31:30 -0300

    lint

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,12 +1,10 @@
-const { token } = require('../middlewares/token');
 const { authService } = require('../services');
 const { mapStatus } = require('../utils/mapStatus');
 
 const auth = async ({ body: { email, password } }, res) => {
   try {
     const { type, message } = await authService.auth({ email, password });
-    if (type) return res.status(mapStatus(type)).json(message);
-    res.status(mapStatus('OK')).json({ token: token(email) });
+    return res.status(mapStatus(type)).json(message);
   } catch (err) {
     console.log(err);
     return res
